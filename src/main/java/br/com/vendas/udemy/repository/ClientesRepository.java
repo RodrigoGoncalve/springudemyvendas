0b98b00f7a908eb01d66df25ec6dddf4e4d8f65c
Rodrigo Gonçalves <rodrigo.goncalves@squadra.com.br>
2022-08-08 10:42:55 -0300

    configuração do proporties

---
+++ src/main/java/br/com/vendas/udemy/repository/ClientesRepository.java
@@ -0,0 +1,7 @@
+package br.com.vendas.udemy.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+}
---
+++ src/main/java/br/com/vendas/udemy/service/ClientesService.java
@@ -0,0 +1,9 @@
+package br.com.vendas.udemy.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientesService {
+
+
+}
